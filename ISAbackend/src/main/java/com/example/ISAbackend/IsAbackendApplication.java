8ac71cd6adbb4e9202d539cb43df595d4212e66a
AleksandraVujovic <vujovic.ra103.2019@uns.ac.rs>
2022-10-25 12:00:58 +0200

    Add frontend and backend projects

---
+++ ISAbackend/src/main/java/com/example/ISAbackend/IsAbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ISAbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsAbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsAbackendApplication.class, args);
+	}
+
+}
