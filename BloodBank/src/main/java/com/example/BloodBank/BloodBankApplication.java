42af3572a1227edf1c341fb0aa33606536085dc6
mihajlo-ra92 <maksimovic.ra92.2019@uns.ac.rs>
2022-11-07 00:49:35 +0100

    Implement send message to queue on schedule

--- BloodBank/src/main/java/com/example/BloodBank/BloodBankApplication.java
+++ BloodBank/src/main/java/com/example/BloodBank/BloodBankApplication.java
@@ -4,6 +4,7 @@ import com.example.BloodBank.model.BloodBank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
 
+@EnableScheduling
 @SpringBootApplication
 public class BloodBankApplication {
 
