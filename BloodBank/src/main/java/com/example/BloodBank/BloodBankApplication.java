8ac453d00f658063627489724ba92aa8375c8fa9
mihajlo-ra92 <94182463+mihajlo-ra92@users.noreply.github.com>
2022-11-11 12:49:41 +0100

    Revert "Send to mq"

--- BloodBank/src/main/java/com/example/BloodBank/BloodBankApplication.java
+++ BloodBank/src/main/java/com/example/BloodBank/BloodBankApplication.java
@@ -1,35 +1,13 @@
 package com.example.BloodBank;
 
-import com.example.BloodBank.model.BloodBank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.ArrayList;
-
-@EnableScheduling
 @SpringBootApplication
 public class BloodBankApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BloodBankApplication.class, args);
-
-	}
-	// da bismo slali headere sa fronta na back
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/**").allowedOrigins("http://localhost:4200");
-			}
-		};
 	}
 
 }
