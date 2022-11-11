96e21b6628c20b0ceb268ade052deec3c43e6cde
mihajlo-ra92 <maksimovic.ra92.2019@uns.ac.rs>
2022-11-11 12:55:46 +0100

    Merge into develop

--- BloodBank/src/main/java/com/example/BloodBank/BloodBankApplication.java
+++ BloodBank/src/main/java/com/example/BloodBank/BloodBankApplication.java
@@ -13,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
 
+//vrv ce se izbaciti posle
 @EnableScheduling
 @SpringBootApplication
 public class BloodBankApplication {
