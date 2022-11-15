afcfc03a7cbd88cc44c470b826a5401232a47e3a
Halid Pasanovic <halidpasanovic1000@gmail.com>
2022-11-15 21:32:28 +0100

    Fixing creation of customer

--- BloodBank/src/main/java/com/example/BloodBank/controller/CustomerController.java
+++ BloodBank/src/main/java/com/example/BloodBank/controller/CustomerController.java
@@ -49,7 +49,7 @@ public class CustomerController {
         try {
             customerService.registerCustomer(newCustomer);
         } catch (Exception e) {
-            return new ResponseEntity<>(e, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(HttpStatus.OK);
     }
