648933424816549d1ba96ae5a31d240aabd25428
AleksandraVujovic <vujovic.ra103.2019@uns.ac.rs>
2022-12-22 20:15:16 +0100

    Create Head Admin Controller and Service

---
+++ BloodBank/src/main/java/com/example/BloodBank/controller/HeadAdminController.java
@@ -0,0 +1,25 @@
+package com.example.BloodBank.controller;
+
+import adapters.CustomerMapper;
+import com.example.BloodBank.service.AdminService;
+import com.example.BloodBank.service.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/head_admin")
+@CrossOrigin(origins = "http://localhost:4200")
+public class HeadAdminController {
+    private final AdminService adminService;
+    private final ModelMapper modelMapper;
+    //private CustomerMapper customerMapper;
+    @Autowired
+    public HeadAdminController(AdminService adminService, ModelMapper modelMapper){
+        this.adminService = adminService;
+        this.modelMapper = modelMapper;
+        //this.customerMapper = new CustomerMapper(modelMapper);
+    }
+}
